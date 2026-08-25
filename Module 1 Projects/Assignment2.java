cb161b8907d96da7d41b0393c7aaacedc020983a
Derek Holbrook <derekholbrook95@protonmail.com>
2026-08-25 17:23:27 -0400

    Add Assignment2.java to calculate sum of numbers

---
+++ Module 1 Projects/Assignment2.java
@@ -0,0 +1,9 @@
+public class Assignment2 
+{
+    public static void main(String[] args) 
+    {
+    String phrase = "The sum of 1, 2, and 3 is ";
+    int sum = 1 + 2 + 3;
+    System.out.println(phrase + sum); 
+    }
+}
