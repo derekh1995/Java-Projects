b78f531555d94a1375caa4097994217c3da9264a
Derek Holbrook <derekholbrook95@protonmail.com>
2026-09-20 18:15:41 -0400

    Create Assignment6.java to print numbers 1 to 100
    Implement a Java program that prints numbers from 1 to 100 using a for loop.

---
+++ Module 2 Projects/Assignment6.java
@@ -0,0 +1,17 @@
+//Derek Holbrook
+
+package Module2;
+
+public class Assignment6 
+{
+    public static void main(String[] args) 
+    {
+        //I chose the "for" loop because when I was learning Python, I was taught that "for" loops are generally better to use for counting integers and "while" loops are better suited for other tasks, like looping gameplay.
+        for(int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+//Notes:
+//"i++" can also be written as "i += 1" or "i = i + 1"
