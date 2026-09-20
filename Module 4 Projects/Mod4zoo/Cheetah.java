91e686010eec517827900dc45142c1658c0002f6
Derek Holbrook <derekholbrook95@protonmail.com>
2026-09-20 18:23:44 -0400

    Add Cheetah class extending Mammal

---
+++ Module 4 Projects/Mod4zoo/Cheetah.java
@@ -0,0 +1,19 @@
+// Derek Holbrook
+// Assignment 17
+
+package Module4.Mod4zoo;
+
+public class Cheetah extends Mammal {
+
+    private int age;
+
+    public Cheetah(int age) {
+        super("meow");
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+}
